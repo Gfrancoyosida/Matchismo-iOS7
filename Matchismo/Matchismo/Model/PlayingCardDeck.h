4503e0c55bae772a198f8a5438016c14ff00aa48
Gfrancoyosida <gianfranco@vchavez.local>
2015-09-10 14:27:39 -0500

    adding more entities to the model

---
+++ Matchismo/Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Gianfranco on 9/10/15.
+//  Copyright (c) 2015 Gianfranco. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
---
+++ Matchismo/Matchismo/Model/PlayingCardDeck.m
@@ -0,0 +1,25 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by Gianfranco on 9/10/15.
+//  Copyright (c) 2015 Gianfranco. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+
+@implementation PlayingCardDeck
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+//        for (NSString *suit in [PlayingCard validSuits]) {
+            
+  //      }
+    }
+    return self;
+}
+
+@end

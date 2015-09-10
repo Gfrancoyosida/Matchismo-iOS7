fe2ac2dbf196595333785da99670fae336efa6a4
Gfrancoyosida <gianfranco@vchavez.local>
2015-09-10 13:59:55 -0500

    adding PlayingCard's methods

--- Matchismo/Matchismo/Model/PlayingCard.m
+++ Matchismo/Matchismo/Model/PlayingCard.m
@@ -10,4 +10,35 @@
 
 @implementation PlayingCard
 
+@synthesize suit = _suit;
+
+- (void) setSuit:(NSString *)suit {
+    if ([[PlayingCard validSuits] containsObject:suit]) {
+        _suit = suit;
+    }
+}
+
+- (NSString *) suit {
+    return _suit? _suit : @"?";
+}
+
+- (NSString *) contents {
+    NSArray *ranks = [PlayingCard rankStrings];
+    NSString *currentRank = ranks[self.rank];
+    return [currentRank stringByAppendingString:self.suit];
+}
+
++ (NSArray *) rankStrings {
+    return @[@"?", @"A", @"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"];
+}
+
++ (NSArray *)validSuits {
+    return @[@"♥︎", @"♣︎", @"♠︎", @"♦︎"];
+}
+
++ (NSUInteger)maxRank {
+    return [[self rankStrings] count] - 1;
+}
+
+
 @end
